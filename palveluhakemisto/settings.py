a990880fd8feb23fb4ab0ce27b546291717b281a
user <tsb358@gmail.com>
2017-07-31 15:13:53 +0300

    settings conf

--- palveluhakemisto/settings.py
+++ palveluhakemisto/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'services',
-    'floppyforms',
     'rest_framework',
     'taggit',
     'taggit_serializer'
